2a6b512093935c740a1d089aab7ac256a60e40a3
= <=>
2026-06-29 12:58:41 +0200

    implementing doubly linked list

---
+++ Doubly Linked Lists/src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+
+    int data;
+    Node prevNode;
+    Node nextNode;
+
+    public Node(int data){
+        this.data = data;
+        prevNode = null;
+        nextNode = null;
+    }
+}
